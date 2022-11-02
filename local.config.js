cd9341bbc01f4fe6cc2091a2a00aee64cfc6eb87
myz1237 <mengyizhuoprivate@outlook.com>
2022-11-02 22:30:20 +0800

    add local version

---
+++ local.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	apps: [
+		{
+			name: 'eden_local',
+			autorestart: true,
+			exec_mode: 'fork',
+			watch: true,
+			script: './dist/index.js',
+			error_file: '~/.pm2/logs/eden_local-error.log',
+			out_file: '~/.pm2/logs/eden_local-out.log',
+			ignore_watch: ['node_modules', 'src'],
+			env: {
+				PM2_ENDPOINT: 'https://oasis-botdevelopment.up.railway.app/graphql',
+				PM2_FRONTEND: 'https://eden-develop.vercel.app',
+				PM2_MODE: 'dev'
+			}
+		}
+	]
+};
